1ad44f3320be8b166cd33f89f9e597b114a07080
SantiagoA-T <80431562+SantiagoA-T@users.noreply.github.com>
2021-05-17 22:55:14 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+print("Bienvenidos a nuestro maravilloso entorno web!!!!")
+
+#variables
+                                                                        #numeros = [n for n in range(10, 12) if +1]  
+respuesta_else = ("Vuelve en")
+numeros = [1, 2, 3, 4, 5]
+pregunta = input("Nombre y apellido:")
+print("Hola, Santiago")
+pregunta_edad = int(input("¿Que edad tienes?"))
+
+if pregunta_edad >= 20:
+    print("Bienvenido!!!")
+else:
+    print(respuesta_else)
+
+def calculo():
+    if pregunta_edad == 19:
+        print(numeros[0] , 'año')
+    
+def calculo2():
+    if pregunta_edad == 18:
+        print(numeros[1] , 'años')  
+
+def calculo3():
+    if pregunta_edad == 17:
+        print(numeros[2] , 'años')  
+
+def calculo4():
+    if pregunta_edad == 16:
+        print(numeros[3] , 'años') 
+
+def calculo5():
+    if pregunta_edad == 15:
+        print(numeros[4] , 'años')  
+ 
+calculo()
+calculo2()
+calculo3()
+calculo4()
+calculo5()
+
+"""while  < 20:
+    print(respuesta_else_3)
+    respuesta_else_4 += 1"""
+
+#def ejemplo()   
+#condiciones
+# if, else, elif
